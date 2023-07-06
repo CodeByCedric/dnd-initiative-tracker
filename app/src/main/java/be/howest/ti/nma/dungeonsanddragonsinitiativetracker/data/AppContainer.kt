3bf18fd36fe47b18ff0525292ba9aafc591e1229
cedric.puystjens <cedric.puystjens@student.howest.be>
2023-07-06 15:58:49 +0200

    implement AppContainer

--- app/src/main/java/be/howest/ti/nma/dungeonsanddragonsinitiativetracker/data/AppContainer.kt
+++ app/src/main/java/be/howest/ti/nma/dungeonsanddragonsinitiativetracker/data/AppContainer.kt
@@ -1,11 +1,18 @@
 package be.howest.ti.nma.dungeonsanddragonsinitiativetracker.data
 
 import android.content.Context
+import be.howest.ti.nma.dungeonsanddragonsinitiativetracker.data.db.DnDInitiativeTrackerDatabase
+import be.howest.ti.nma.dungeonsanddragonsinitiativetracker.data.db.repositories.interfaces.CampaignRepository
+import be.howest.ti.nma.dungeonsanddragonsinitiativetracker.data.db.repositories.offlinerepositories.OfflineCampaignRepository
 
 interface AppContainer {
+    val campaignRepository: CampaignRepository
 }
 
 class AppDataContainer(private val context: Context) : AppContainer {
+    override val campaignRepository: CampaignRepository by lazy {
+        OfflineCampaignRepository(DnDInitiativeTrackerDatabase.getDatabase(context).CampaignDao())
+    }
 
 
 }
